b1e35bf9847d18056e1ee37e5056bd0dfd4edd51
askapse <askapse99@gmail.com>
2023-08-12 01:34:12 +0530

    Add contact page title

--- plus-academia/web/src/components/contact/Contact.tsx
+++ plus-academia/web/src/components/contact/Contact.tsx
@@ -4,6 +4,7 @@ import ContactForm from "./ContactForm";
 function Contact() {
   return (
     <div className="contact">
+      <h1>Contact Us</h1>
       <ContactForm />
       <ContactDetails />
     </div>
